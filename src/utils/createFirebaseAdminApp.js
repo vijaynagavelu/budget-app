7bbd6bca3cdbb45e13f4175b5296cfeb5410ca2e
Vijay <vijaynaga.0503@gmail.com>
2023-08-17 12:59:25 +0530

    git

--- src/utils/createFirebaseAdminApp.js
+++ src/utils/createFirebaseAdminApp.js
@@ -1,15 +1,15 @@
 import admin from 'firebase-admin';
-import serviceAccount from "../serviceAccountKey.json";
+import serviceAccount from "../serviceAccountKey";
 
 export function createFirebaseAdminApp() {
-    
-    // if already created, return the same instance
-    if (admin.apps.length > 0) {
-      return admin.app();
-    }
 
-    // initialize admin app
-    return admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-          });
-        }
+  // if already created, return the same instance
+  if (admin.apps.length > 0) {
+    return admin.app();
+  }
+
+  // initialize admin app
+  return admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
