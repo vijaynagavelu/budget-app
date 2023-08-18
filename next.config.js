babd3da757eeff412353deefae8a59891a0b998d
Vijay <vijaynaga.0503@gmail.com>
2023-08-18 13:38:57 +0530

    CORS enabled

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 module.exports = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    },
     images: {
-        domains: ["cdn.pixabay.com", "www.pngall.com", "cdn-icons-png.flaticon.com","media.tenor.com"],
+        domains: ["cdn.pixabay.com", "www.pngall.com", "cdn-icons-png.flaticon.com", "media.tenor.com"],
         deviceSizes: [670, 750, 828, 1080, 1224, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
     }
